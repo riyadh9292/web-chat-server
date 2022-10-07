33bfbf5be25d288ef21ea845640f5a1976c5cf26
riyadh <mrriyadh66@gmail.com>
2022-10-07 21:40:00 +0600

    backend done

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const app = express();
 const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
-
 app.use(cors());
+
 const server = http.createServer(app);
+
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
@@ -14,12 +15,22 @@ const io = new Server(server, {
 });
 
 io.on("connection", (socket) => {
-  console.log("connected user", socket.id);
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
   socket.on("disconnect", () => {
-    console.log("user Disconnect", socket.id);
+    console.log("User Disconnected", socket.id);
   });
 });
 
 server.listen(3001, () => {
-  console.log("server is running");
+  console.log("SERVER RUNNING");
 });
